7a85c3244d9f4475034f692526f763afd6d6ecd9
Yuls666 <130798272+Yuls666@users.noreply.github.com>
2023-08-03 11:26:23 -0500

    new escena

---
+++ Assets/MenuInicia.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicia : MonoBehaviour
+{
+   public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("SALIR....");
+        Application.Quit();
+
+    }
+}
